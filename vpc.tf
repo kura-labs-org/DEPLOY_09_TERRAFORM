bfa9c325a4ddbefe02192e775d8b3b2627dcf50f
KennethT404 <tankenneth98@gmail.com>
2021-12-04 13:20:49 -0500

    updates to vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,29 @@ resource "aws_vpc" "main" {
     Name = "Main VPC"
   }
 }
+
+resource "aws_subnet" "public01" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "Public01"
+  }
+}
+
+resource "aws_subnet" "public02" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "Public02"
+  }
+}
+
+resource "aws_internet_gateway" "ig1" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "ig1"
+  }
+}

162b9bbe6a902fba28318eb94784c46f1c062c9a
Kawang Wong <nycazn724@gmail.com>
2021-12-04 13:06:15 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -60,3 +60,16 @@ resource "aws_internet_gateway" "ig1" {
     
   }
 }
+
+resource "aws_route_table" "routetable1" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+  cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.ig1.id
+  }
+
+  tags = {
+    "Name" = "RouteTable1"
+  }
+}

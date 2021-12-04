848e53ab09005c21058a2dc272ef17ecc942b34d
Zach Cyrus <zscyrus31@gmail.com>
2021-12-04 15:03:26 -0500

    Addeded private RT as well route associations

--- vpc.tf
+++ vpc.tf
@@ -47,9 +47,26 @@ resource "aws_internet_gateway" "ig1" {
   }
 }
 
+### Nat Gateway
+
+resource "aws_nat_gateway" "nat-gateway" {
+  allocation_id = aws_eip.nat_elastic_ip.id
+  subnet_id = aws_subnet.public01.id
+
+
+  
+}
+
+#### Elastic IP for Nat Gateway
+
+resource "aws_eip" "nat_elastic_ip" {
+  vpc = true
+}
+
 ### Route Table
 
-resource "aws_route_table" "project1-route-table" {
+# Public RT
+resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -60,4 +77,37 @@ resource "aws_route_table" "project1-route-table" {
   tags = {
     "Name" = "main-vpc-table"
   }
+}
+
+# Private RT
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gateway.id
+  }
+
+  tags = {
+    "Name" = "Private Route Table for Deploy9"
+  }
+}
+
+### Configuring route table for public
+
+resource "aws_route_table_association" "main_vpc_public01_subnet_route" {
+    subnet_id = aws_subnet.public01.id
+    route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_route_table_association" "main_vpc_public02_subnet_route" {
+    subnet_id = aws_subnet.public02.id
+    route_table_id = aws_route_table.public-route-table.id
+}
+
+### Configuring route table for private
+
+resource "aws_route_table_association" "main_vpc_private01_subnet_route" {
+    subnet_id = aws_subnet.private01.id
+    route_table_id = aws_route_table.private-route-table.id
 }

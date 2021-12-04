1d64e982deadf0a0a3d917ba8f843aeb8e2d5498
Brittney jones <brittneyjones@Brittneys-MacBook-Pro.local>
2021-12-04 13:19:00 -0500

    updating vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,7 @@
 # VPC
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
+
+# creating VPC "Main"
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/18"
 
@@ -7,3 +9,53 @@ resource "aws_vpc" "main" {
     Name = "Main VPC"
   }
 }
+
+resource "aws_subnet" "public01" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Public01"
+  }
+}
+
+resource "aws_subnet" "public02" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Public02"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "deploy9_igw1"
+  }
+}
+
+resource "aws_route_table" "route-table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "deploy09_rt"
+  }
+}
+
+# resource "aws_subnet" "private01" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = "10.0.2.0/24"
+#   availability_zone = "us-east-1b"
+
+#   tags = {
+#     Name = "Public02"
+#   }
+# }

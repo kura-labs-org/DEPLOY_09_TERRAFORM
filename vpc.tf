90bec43b76ab8cb8c3e2b3c03395adb8f8cae2d8
Zach Cyrus <zscyrus31@gmail.com>
2021-12-04 13:43:37 -0500

    Formatted .tf files with fmt

--- vpc.tf
+++ vpc.tf
@@ -10,27 +10,27 @@ resource "aws_vpc" "main" {
 
 ### Subnets
 
-resource "aws_subnet" "public01"{
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "public01" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
   tags = {
     Name = "Public01"
   }
 }
 
-resource "aws_subnet" "public02"{
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+resource "aws_subnet" "public02" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
   tags = {
     Name = "Public02"
   }
 }
 
-resource "aws_subnet" "private01"{
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.3.0/24"
+resource "aws_subnet" "private01" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1a"
   tags = {
     Name = "Private01"
@@ -53,8 +53,8 @@ resource "aws_route_table" "project1-route-table" {
   vpc_id = aws_vpc.main.id
 
   route {
-  cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.ig1.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig1.id
   }
 
   tags = {

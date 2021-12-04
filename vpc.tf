b5ed387e8f7cae4017831c7143a763cf9ef9cc28
Sai <saihoyip@gmail.com>
2021-12-04 16:18:31 -0500

    What I have so far

--- vpc.tf
+++ vpc.tf
@@ -29,6 +29,39 @@ resource "aws_subnet" "public02" {
   }
 }
 
+resource "aws_subnet" "Private01" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    "Name" = "Private01"
+  }
+}
+
+resource "aws_subnet" "internal01" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name" = "internal01"
+  }
+}
+
+resource "aws_subnet" "internal02" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.5.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name" = "internal02"
+  }
+}
+
 resource "aws_internet_gateway" "ig1" {
   vpc_id = aws_vpc.main.id
   tags = {
@@ -45,5 +78,89 @@ resource "aws_route_table" "project1-route-table" {
 
   tags = {
     "Name" = "project1-route-table"
+  }
+}
+
+resource "aws_security_group" "allow_tcp" {
+  name        = "allow_tcp"
+  description = "Allow TCP inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TCP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tcp"
+  }
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = "ami-09e67e426f25ce0d7"
+  instance_type          = "t2.micro"
+  key_name               = "saikey"
+  monitoring             = true
+  vpc_security_group_ids = [aws_security_group.allow_tcp.id]
+  subnet_id              = aws_subnet.Private01.id
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_security_group" "lb_SG" {
+  name        = "lb_SG"
+  description = "security group for load balancer"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TCP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "-1"
+    cidr_blocks = [aws_security_group.allow_tcp.id]
+  }
+
+  tags = {
+    Name = "lb_SG"
+  }
+}
+
+
+resource "aws_lb" "deploy9_lb" {
+  name               = "deploy9_lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = aws_subnet.public.*.id
+
+  enable_deletion_protection = true
+
+  tags = {
+    Environment = "production"
   }
 }

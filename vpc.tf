b474414b45ec4ea17914e564d80c2526e0a7e807
SrKoDes <desravines.kohiin@gmail.com>
2021-12-04 13:35:19 -0500

    first commit

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,46 @@ resource "aws_vpc" "main" {
     Name = "Main VPC"
   }
 }
+
+# 1. Create two public subnets
+resource "aws_subnet" "public01" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+  tags = {
+    Name = "PubSub01"
+  }
+}
+
+resource "aws_subnet" "public02" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-2b"
+  tags = {
+    Name = "PubSub02"
+  }
+}
+
+# 2. Create IG
+resource "aws_internet_gateway" "IG" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "IG"
+  }
+}
+
+# 3. Create Custom Route Table
+resource "aws_route_table" "RT1" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+  cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.IG.id
+  }
+
+  tags = {
+    "Name" = "RT1"
+  }
+}
+

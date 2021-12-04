43f1dd4cb915b8c966c79d6a242346b882842d29
Zach Cyrus <zscyrus31@gmail.com>
2021-12-04 15:44:39 -0500

    Update code

---
+++ ec2.tf
@@ -0,0 +1,47 @@
+resource "aws_instance" "jenkins_ec2" {
+  //This is the AMI for Ubuntu in US east 1
+  ami           = "ami-083654bd07b5da81d"
+  instance_type = "t2.micro"
+
+  key_name        = "east1key.pem"
+  security_groups = ["${aws_security_group.jenkins_ec2.name}"]
+
+  subnet_id = aws_subnet.public01.id
+
+  associate_public_ip_address = true
+
+
+
+  tags = {
+    Name = "Ubuntu-Terraform"
+  }
+
+}
+
+
+   
+resource "aws_security_group" "ubuntu_ec2" {
+  name        = "ubuntu-deploy08-sg"
+  description = "Ubuntu security group for terraform"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}

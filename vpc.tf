590e37f97e130f338a9a1be015646c3b0467630e
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2021-12-07 13:57:10 -0500

    Add files via upload

--- vpc.tf
+++ vpc.tf
@@ -153,7 +153,7 @@ resource "aws_security_group" "lb_SG" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [aws_security_group.allow_tcp.id]
   }
 
   tags = {
@@ -161,16 +161,65 @@ resource "aws_security_group" "lb_SG" {
   }
 }
 
-
 resource "aws_lb" "deploy9_lb" {
   name                       = "deploy9-lb"
   internal                   = false
   load_balancer_type         = "application"
   security_groups            = [aws_security_group.lb_SG.id]
   subnets                    = [aws_subnet.public01.id, aws_subnet.public02.id]
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
   tags = {
     Environment = "production"
   }
 }
+
+resource "aws_security_group" "db_SG" {
+  name        = "db_SG"
+  description = "security group for load balancer"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TCP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [aws_security_group.allow_tcp.id]
+  }
+}
+
+  module "db" {
+    source  = "terraform-aws-modules/rds/aws"
+    version = "~> 3.0"
+
+    identifier = "saidb"
+
+    engine            = "mysql"
+    instance_class    = "db.t2.large"
+    allocated_storage = 5
+
+    name     = "example_db"
+    username = "user"
+    password = "YourPwdShouldBeLongAndSecure!"
+    port     = "3306"
+    multi_az = true
+    iam_database_authentication_enabled = true
+
+    vpc_security_group_ids = [aws_security_group.db_SG.id]
+
+    maintenance_window = "Mon:00:00-Mon:03:00"
+    backup_window      = "03:00-06:00"
+
+    tags = {
+      Owner       = "user"
+      Environment = "dev"
+    }
+
+    # DB subnet group
+    subnet_ids = [aws_subnet.internal01.id, aws_subnet.internal02.id]
+
+    # Database Deletion Protection
+    deletion_protection = false
+  }
+
+
